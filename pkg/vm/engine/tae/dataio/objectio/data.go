6317439d9c9fc01895487dd1d6a53e25eaf16072
GreatRiver <2552853833@qq.com>
2022-07-11 11:51:35 +0800

    Fix objectio overwrite bug

--- pkg/vm/engine/tae/dataio/objectio/data.go
+++ pkg/vm/engine/tae/dataio/objectio/data.go
@@ -97,7 +97,10 @@ func (df *dataFile) Read(buf []byte) (n int, err error) {
 	}
 	file := df.GetFile()
 	n, err = file.Read(buf)
-	return n, nil
+	if err != nil {
+		return
+	}
+	return
 }
 
 func (df *dataFile) GetFileCnt() int {
