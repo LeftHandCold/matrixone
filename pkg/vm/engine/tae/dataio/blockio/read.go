1843c2369926f76498540cae92a975f95d9a03b9
GreatRiver <2552853833@qq.com>
2023-03-25 17:59:06 +0800

    workaround for fix #8684

--- pkg/vm/engine/tae/dataio/blockio/read.go
+++ pkg/vm/engine/tae/dataio/blockio/read.go
@@ -16,7 +16,10 @@ package blockio
 
 import (
 	"context"
+	"fmt"
+	"github.com/RoaringBitmap/roaring"
 	"github.com/matrixorigin/matrixone/pkg/container/nulls"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/catalog"
 	"time"
 
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
@@ -44,17 +47,23 @@ func BlockRead(
 	pool *mpool.MPool) (*batch.Batch, error) {
 
 	// read
-	columnBatch, err := BlockReadInner(
+	columnBatch, err := BlockReadInner2(
 		ctx, info, colIdxes, colTypes,
 		types.TimestampToTS(ts), fs, pool,
 	)
 	if err != nil {
 		return nil, err
 	}
+	bat := batch.NewWithSize(len(colIdxes))
+	for i, vec := range columnBatch.Vecs {
+		// FIXME: CN may modify the vector, so it must be copied here now
+		bat.Vecs[i] = containers.CopyToMoVec(vec)
+		vec.Close()
+	}
 
-	columnBatch.SetZs(columnBatch.Vecs[0].Length(), pool)
+	bat.SetZs(bat.Vecs[0].Length(), pool)
 
-	return columnBatch, nil
+	return bat, nil
 }
 
 func BlockReadInner(
@@ -259,4 +268,194 @@ func recordDeletes(deleteBatch *batch.Batch, ts types.TS) []int64 {
 		rows = append(rows, int64(r))
 	}
 	return rows
+}
+
+func BlockReadInner2(
+	ctx context.Context,
+	info *pkgcatalog.BlockInfo,
+	colIdxes []uint16,
+	colTypes []types.Type,
+	ts types.TS,
+	fs fileservice.FileService,
+	pool *mpool.MPool) (*containers.Batch, error) {
+	columnBatch, err := readBlockData2(ctx, colIdxes, colTypes, info, ts,
+		fs, pool)
+	if err != nil {
+		return nil, err
+	}
+	if info.DeltaLoc != "" {
+		deleteBatch, err := readBlockDelete2(ctx, info.DeltaLoc, fs)
+		if err != nil {
+			return nil, err
+		}
+		recordDeletes2(columnBatch, deleteBatch, ts)
+		logutil.Infof(
+			"blockread %d read delete %d: base %s filter out %v\n",
+			info.BlockID, deleteBatch.Length(), ts.ToString(), columnBatch.DeleteCnt())
+		deleteBatch.Close()
+	}
+	// remove rows from columns
+	if columnBatch.Deletes != nil {
+		for i, col := range columnBatch.Vecs {
+			columnBatch.Vecs[i] = col.CloneWindow(0, col.Length(), nil)
+			columnBatch.Vecs[i].Compact(columnBatch.Deletes)
+			col.Close()
+		}
+	}
+	return columnBatch, nil
+}
+
+func readBlockData2(ctx context.Context, colIndexes []uint16,
+	colTypes []types.Type, info *pkgcatalog.BlockInfo, ts types.TS,
+	fs fileservice.FileService, m *mpool.MPool) (*containers.Batch, error) {
+	ok, _, idxes := getRowsIdIndex(colIndexes, colTypes)
+	_, id, extent, rows, err := DecodeLocation(info.MetaLoc)
+	if err != nil {
+		return nil, err
+	}
+	reader, err := NewObjectReader(fs, info.MetaLoc)
+	if err != nil {
+		return nil, err
+	}
+	var rowIdVec containers.Vector
+	bat := containers.NewBatch()
+	defer func() {
+		if err != nil {
+			bat.Close()
+		}
+	}()
+	if ok {
+		// generate rowIdVec
+		prefix := info.BlockID[:]
+		rowIdVec, err = model.PreparePhyAddrDataWithPool(
+			types.T_Rowid.ToType(),
+			prefix,
+			0,
+			rows,
+			m,
+		)
+		if err != nil {
+			return nil, err
+		}
+		defer func() {
+			if err != nil {
+				rowIdVec.Close()
+			}
+		}()
+	}
+
+	loadBlock := func(idxes []uint16) ([]*batch.Batch, error) {
+		if len(idxes) == 0 && ok {
+			// only read rowid column on non appendable block, return early
+			bat.AddVector(catalog.AttrRowID, rowIdVec)
+			return nil, nil
+		}
+		bats, err := reader.LoadColumns(ctx, idxes, []uint32{id}, m)
+		if err != nil {
+			return nil, err
+		}
+		entry := bats[0].Vecs
+		for i, typ := range colTypes {
+			if typ.Oid == types.T_Rowid {
+				bat.AddVector(fmt.Sprintf("%d", i), rowIdVec)
+			} else {
+				bat.AddVector(fmt.Sprintf("%d", i),
+					containers.NewVectorWithSharedMemory(entry[0], true))
+				entry = entry[1:]
+			}
+		}
+		return bats, nil
+	}
+
+	loadAppendBlock := func() error {
+		// appendable block should be filtered by committs
+		blocks, err := reader.(*BlockReader).reader.ReadMeta(ctx, []objectio.Extent{extent}, m, LoadZoneMapFunc)
+		if err != nil {
+			return err
+		}
+
+		colCount := blocks[0].GetColumnCount()
+		idxes = append(idxes, colCount-2) // committs
+		idxes = append(idxes, colCount-1) // aborted
+		bats, err := loadBlock(idxes)
+		if err != nil {
+			return err
+		}
+		lenVecs := len(bats[0].Vecs)
+		t0 := time.Now()
+		v1 := bats[0].Vecs[lenVecs-2]
+		commits := containers.NewVectorWithSharedMemory(v1, false)
+		defer commits.Close()
+		v2 := bats[0].Vecs[lenVecs-1]
+		abort := containers.NewVectorWithSharedMemory(v2, false)
+		defer abort.Close()
+		for i := 0; i < commits.Length(); i++ {
+			if abort.Get(i).(bool) || commits.Get(i).(types.TS).Greater(ts) {
+				if bat.Deletes == nil {
+					bat.Deletes = roaring.NewBitmap()
+				}
+				bat.Deletes.Add(uint32(i))
+			}
+		}
+		logutil.Infof(
+			"blockread %d scan filter cost %v: base %s filter out %v\n ",
+			info.BlockID, time.Since(t0), ts.ToString(), bat.DeleteCnt())
+		return nil
+	}
+
+	if info.EntryState {
+		err = loadAppendBlock()
+	} else {
+		_, err = loadBlock(idxes)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	return bat, nil
+}
+
+func readBlockDelete2(ctx context.Context, deltaloc string, fs fileservice.FileService) (*containers.Batch, error) {
+	bat := containers.NewBatch()
+	colNames := []string{catalog.PhyAddrColumnName, catalog.AttrCommitTs, catalog.AttrAborted}
+	_, id, _, _, err := DecodeLocation(deltaloc)
+	if err != nil {
+		return nil, err
+	}
+	reader, err := NewObjectReader(fs, deltaloc)
+	if err != nil {
+		return nil, err
+	}
+
+	ioResult, err := reader.LoadColumns(ctx, []uint16{0, 1, 2}, []uint32{id}, nil)
+	if err != nil {
+		return nil, err
+	}
+	for i, entry := range ioResult[0].Vecs {
+		bat.AddVector(colNames[i], containers.NewVectorWithSharedMemory(entry, false))
+	}
+	return bat, nil
+}
+
+func recordDeletes2(columnBatch *containers.Batch, deleteBatch *containers.Batch, ts types.TS) {
+	if deleteBatch == nil {
+		return
+	}
+	// record visible delete rows
+	for i := 0; i < deleteBatch.Length(); i++ {
+		abort := deleteBatch.GetVectorByName(catalog.AttrAborted).Get(i).(bool)
+		if abort {
+			continue
+		}
+		commitTS := deleteBatch.GetVectorByName(catalog.AttrCommitTs).Get(i).(types.TS)
+		if commitTS.Greater(ts) {
+			continue
+		}
+		rowid := deleteBatch.GetVectorByName(catalog.PhyAddrColumnName).Get(i).(types.Rowid)
+		_, _, row := model.DecodePhyAddrKey(rowid)
+		if columnBatch.Deletes == nil {
+			columnBatch.Deletes = roaring.NewBitmap()
+		}
+		columnBatch.Deletes.Add(row)
+	}
 }
