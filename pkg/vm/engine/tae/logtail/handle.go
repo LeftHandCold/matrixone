48282e0a7860cc8273a9b43ed24bbf67eee788af
GreatRiver <2552853833@qq.com>
2023-11-02 00:43:14 +0800

    Update code

--- pkg/vm/engine/tae/logtail/handle.go
+++ pkg/vm/engine/tae/logtail/handle.go
@@ -1163,19 +1163,47 @@ func ReWriteCheckpointAndBlockFromKey(
 					}
 				}
 				commitTs := types.TS{}
+				deleteRow := make([]int64,0)
 				for v := 0; v < bat.Vecs[0].Length(); v++ {
 					err = commitTs.Unmarshal(bat.Vecs[len(bat.Vecs)-2].GetRawBytesAt(v))
 					if err != nil {
 						return nil, nil, nil, nil, err
 					}
 					if commitTs.Greater(ts) {
-						windowCNBatch(bat, 0, uint64(v))
+						for y := v; y < bat.Vecs[0].Length(); y++ {
+							debugcommitTs := types.TS{}
+							err = debugcommitTs.Unmarshal(bat.Vecs[len(bat.Vecs)-3].GetRawBytesAt(y))
+							if err != nil {
+								return nil, nil, nil, nil, err
+							}
+							if debugcommitTs.LessEq(ts) {
+								logutil.Infof("debugcommitTs1 is not sorted %v ts %v, block is %v", debugcommitTs.ToString(), ts.ToString(), block.location.String())
+								//panic("debugcommitTs is not sorted")
+							}
+						}
+						/*windowCNBatch(bat, 0, uint64(v))
 						c := types.TS{}
-						err = c.Unmarshal(bat.Vecs[len(bat.Vecs)-2].GetRawBytesAt(bat.Vecs[0].Length() -1))
+						err = c.Unmarshal(bat.Vecs[len(bat.Vecs)-2].GetRawBytesAt(bat.Vecs[0].Length() -1))*/
 						logutil.Infof("blkCommitTs %v ts %v, c %v , block is %v", commitTs.ToString(), ts.ToString(), c.ToString(), block.location.String())
 						isChange = true
 						isCkpChange = true
-						break
+					} else {
+						deleteRow = append(deleteRow, int64(v))
+					}
+				}
+				if len(deleteRow) > 0 {
+					bat.Shrink(deleteRow)
+					logutil.Infof("deleteRow1 is %d, bat length %d", len(deleteRow), bat.Vecs[0].Length())
+					commitTs1 := types.TS{}
+					for v := 0; v < bat.Vecs[0].Length(); v++ {
+						err = commitTs1.Unmarshal(bat.Vecs[len(bat.Vecs)-3].GetRawBytesAt(v))
+						if err != nil {
+							return nil, nil, nil, nil, err
+						}
+					}
+					if commitTs.Greater(ts) {
+						logutil.Infof("commitTs111 %v ts %v, block is %v", commitTs.ToString(), ts.ToString(), block.location.String())
+						panic("commitTs.Greater(ts2)")
 					}
 				}
 				objectData.data[id].data = bat
