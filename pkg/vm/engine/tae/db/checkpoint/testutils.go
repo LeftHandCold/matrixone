2b4e9328b82fb9d3fc53480522519590dd6efc1e
GreatRiver <2552853833@qq.com>
2023-11-01 15:39:25 +0800

    Update code

--- pkg/vm/engine/tae/db/checkpoint/testutils.go
+++ pkg/vm/engine/tae/db/checkpoint/testutils.go
@@ -208,7 +208,7 @@ func (r *runner) ForceCheckpointForBackup(end types.TS) (location string, err er
 	}
 	backupTime := time.Now().UTC()
 	currTs := types.BuildTS(backupTime.UnixNano(), 0)
-	logutil.Infof("backup time is %v", currTs.String())
+	logutil.Infof("backup time is %v", currTs.ToString())
 	backup := NewCheckpointEntry(end.Next(), currTs, ET_Incremental)
 	location, err = r.doCheckpointForBackup(backup)
 	if err != nil {
