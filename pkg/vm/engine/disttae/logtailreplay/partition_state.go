b6834c1499f93782fb09a40f62cb42b90f6be76d
GreatRiver <2552853833@qq.com>
2024-05-09 16:47:18 +0800

    Update code

--- pkg/vm/engine/disttae/logtailreplay/partition_state.go
+++ pkg/vm/engine/disttae/logtailreplay/partition_state.go
@@ -755,9 +755,11 @@ func (p *PartitionState) HandleMetadataInsert(
 			commitTS := commitTSVec[y]
 			if blockid.String() == "018f5c41-8124-745c-b336-f87eef02323a-0-40" {
 				logutil.Infof("HandleMetadataInsert blockid2: %v, deltaLoc: %v, commitTS: %v, length: %d", blockid.String(), deltaLoc.String(), commitTS.ToString(), mustVectorFromProto(input.Vecs[2]).Length())
+				logutil.Infof("blockIDVector is %d, 68 is %v", len(blockIDVector), blockIDVector[68].String())
 			}
 		}
 	}
+	defer logutil.Infof("HandleMetadataInsert is done")
 	for i, blockID := range blockIDVector {
 		if blockID.String() == "018f5c41-8124-745c-b336-f87eef02323a-0-40" {
 			logutil.Infof("update delta location111 %s, blockid %v, commitTimeVector[i] is %v", objectio.Location(deltaLocationVector.GetBytesAt(i)).String(), blockID.String(), commitTimeVector[i].ToString())
