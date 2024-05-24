8390282f8e1f3e39513a1651e2206085bef7c4c2
GreatRiver <2552853833@qq.com>
2024-05-24 18:56:56 +0800

    Update code

--- pkg/vm/engine/tae/blockio/writer.go
+++ pkg/vm/engine/tae/blockio/writer.go
@@ -114,11 +114,11 @@ func (w *BlockWriter) WriteBatch(batch *batch.Batch) (objectio.BlockObject, erro
 							v.GetByteSlice(area2)
 						}
 					}
-				}
-				for y, v := range slice {
-					svlen := v[0]
-					if svlen > types.VarlenaInlineSize {
-						logutil.Infof("\nvec: %v, \nvec2: %v, v : %v, v2: %v", len(slice), len(slice1), slice[y][0], slice1[y][0], len(area), len(area1))
+					for y, v := range slice {
+						svlen := v[0]
+						if svlen > types.VarlenaInlineSize {
+							logutil.Infof("\nvec: %v, \nvec2: %v, %v  v : %v, v2: %v, v3: %v", len(slice), len(slice1), len(slice2), slice[y][0], slice1[y][0], slice2[y][0], len(area), len(area1), len(area2))
+						}
 					}
 				}
 				panic(r)
