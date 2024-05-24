2c6fdd7a07ebc1152e72d3bd1fbe4a4e42686d33
GreatRiver <2552853833@qq.com>
2024-05-24 18:10:14 +0800

    Update code

--- pkg/vm/engine/tae/blockio/writer.go
+++ pkg/vm/engine/tae/blockio/writer.go
@@ -103,7 +103,7 @@ func (w *BlockWriter) WriteBatch(batch *batch.Batch) (objectio.BlockObject, erro
 			return nil, err
 		}
 		if vec.GetType().IsVarlen() && vec.Length() < 100 {
-			slice, area := movec.MustVarlenaRawData(vec2)
+			slice, area := movec.MustVarlenaRawData(vec)
 			slice = slice[0:vec.Length()]
 			if len(area) == 0 {
 				for _, v := range slice {
