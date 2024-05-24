448d16006e0cf5e8d062d8d19b8d62b856963d25
GreatRiver <2552853833@qq.com>
2024-05-24 18:50:19 +0800

    Update code

--- pkg/vm/engine/tae/blockio/writer.go
+++ pkg/vm/engine/tae/blockio/writer.go
@@ -102,8 +102,8 @@ func (w *BlockWriter) WriteBatch(batch *batch.Batch) (objectio.BlockObject, erro
 		if err != nil {
 			return nil, err
 		}
-		var slice, slice1 []types.Varlena
-		var area, area1 []byte
+		var slice []types.Varlena
+		var area []byte
 		defer func() {
 			if r := recover(); r != nil {
 				if vec.GetType().IsVarlen() && vec.Length() < 100 {
@@ -119,7 +119,6 @@ func (w *BlockWriter) WriteBatch(batch *batch.Batch) (objectio.BlockObject, erro
 		}()
 		if vec.GetType().IsVarlen() && vec.Length() < 100 {
 			slice, area = movec.MustVarlenaRawData(vec2)
-			slice1, area1 = movec.MustVarlenaRawData(vec)
 			slice = slice[0:vec2.Length()]
 			if len(area) == 0 {
 				for _, v := range slice {
