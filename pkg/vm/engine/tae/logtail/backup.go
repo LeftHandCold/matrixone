fc05b254405c0ebc4e52ee0d0ed0e775711758f1
GreatRiver <great.river703@gmail.com>
2024-08-23 14:50:54 +0800

    Update code

--- pkg/vm/engine/tae/logtail/backup.go
+++ pkg/vm/engine/tae/logtail/backup.go
@@ -591,6 +591,7 @@ func ReWriteCheckpointAndBlockFromKey(
 		if bat.Vecs[0].Length() == 0 {
 			continue
 		}
+		bat = formatData(bat)
 		objectData.data = make([]*batch.Batch, 0, 1)
 		objectData.data = append(objectData.data, bat)
 		objectData.isChange = change
