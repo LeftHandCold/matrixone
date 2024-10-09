6594c5a434c694b1e1b73df699b057b304ceb6b4
GreatRiver <great.river703@gmail.com>
2024-10-09 14:49:06 +0800

    Update code

--- pkg/vm/engine/tae/logtail/handle.go
+++ pkg/vm/engine/tae/logtail/handle.go
@@ -480,7 +480,7 @@ func LoadCheckpointEntries(
 
 	for i := range objectLocations {
 		data := NewCNCheckpointData(sid)
-		meteIdxSchema := checkpointDataReferVersions[CheckpointVersion12][MetaIDX]
+		meteIdxSchema := checkpointDataReferVersions[versions[i]][MetaIDX]
 		idxes := make([]uint16, len(meteIdxSchema.attrs))
 		for attr := range meteIdxSchema.attrs {
 			idxes[attr] = uint16(attr)
