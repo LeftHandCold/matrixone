c0b015a414410180a50903aacae7d2f718d857d6
GreatRiver <great.river703@gmail.com>
2024-11-18 22:04:34 +0800

    Update code

--- pkg/vm/engine/tae/txn/txnimpl/anode.go
+++ pkg/vm/engine/tae/txn/txnimpl/anode.go
@@ -130,7 +130,7 @@ func (n *anode) Append(data *containers.Batch, offset uint32) (an uint32, err er
 		// }
 		def := schema.ColDefs[schema.GetColIdx(attr)]
 		destVec := n.data.Vecs[def.Idx]
-		logutil.Infof("destVec: %s, %v, %v,  %d", destVec.String(), data.Vecs[def.Idx].String(), attr, data.Length())
+		logutil.Infof("destVec: %s, %v, %v,  %d, %d %d", destVec.String(), data.Vecs[def.Idx].String(), attr, data.Length(), offset, an)
 		destVec.ExtendWithOffset(data.Vecs[def.Idx], int(offset), int(an))
 	}
 	n.rows = uint32(n.data.Length())
