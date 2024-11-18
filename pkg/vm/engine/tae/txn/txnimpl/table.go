6fccfe48376e3ba7308279bc03af3d93f5476246
GreatRiver <great.river703@gmail.com>
2024-11-18 19:36:43 +0800

    UPdate code

--- pkg/vm/engine/tae/txn/txnimpl/table.go
+++ pkg/vm/engine/tae/txn/txnimpl/table.go
@@ -1547,7 +1547,7 @@ func (tbl *txnTable) DeleteByPhyAddrKeys(
 	if tbl.tombstoneTable == nil {
 		tbl.tombstoneTable = newBaseTable(tbl.entry.GetLastestSchema(true), true, tbl)
 	}
-	logutil.Infof("rowIDVec is %v, pk is %v", rowIDVec.String(), pk.String())
+	logutil.Infof("rowIDVec is %v, pk is %v, deleteBatch is %v", rowIDVec.String(), pk.String(), deleteBatch.String())
 	err = tbl.dedup(tbl.store.ctx, deleteBatch.GetVectorByName(objectio.TombstoneAttr_Rowid_Attr), true)
 	if err != nil {
 		return
