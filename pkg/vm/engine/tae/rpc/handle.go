c84dc488bf424736769aedc03149881539448ec5
GreatRiver <great.river703@gmail.com>
2025-04-14 15:01:03 +0800

    UPdate code

--- pkg/vm/engine/tae/rpc/handle.go
+++ pkg/vm/engine/tae/rpc/handle.go
@@ -17,6 +17,7 @@ package rpc
 import (
 	"context"
 	"fmt"
+	"github.com/matrixorigin/matrixone/pkg/container/vector"
 	"os"
 	"reflect"
 	"regexp"
@@ -933,33 +934,33 @@ func (h *Handle) HandleWrite(
 		}
 
 		// TODO: debug for #13342, remove me later
-		//if h.IsInterceptTable(tb.Schema(false).(*catalog.Schema).Name) {
-		//	schema := tb.Schema(false).(*catalog.Schema)
-		//	if schema.HasPK() {
-		//		pkDef := schema.GetSingleSortKey()
-		//		idx := pkDef.Idx
-		//		isCompositeKey := pkDef.IsCompositeColumn()
-		//		for i := 0; i < req.Batch.Vecs[0].Length(); i++ {
-		//			if isCompositeKey {
-		//				pkbuf := req.Batch.Vecs[idx].GetBytesAt(i)
-		//				tuple, _ := types.Unpack(pkbuf)
-		//				logutil.Info(
-		//					"op1",
-		//					zap.String("txn", txn.String()),
-		//					zap.String("pk", common.TypeStringValue(*req.Batch.Vecs[idx].GetType(), pkbuf, false)),
-		//					zap.Any("detail", tuple.SQLStrings(nil)),
-		//				)
-		//			} else {
-		//				logutil.Info(
-		//					"op1",
-		//					zap.String("txn", txn.String()),
-		//					zap.String("pk", common.MoVectorToString(req.Batch.Vecs[idx], i)),
-		//				)
-		//			}
-		//		}
-		//	}
-		//
-		//}
+		if h.IsInterceptTable(tb.Schema(false).(*catalog.Schema).Name) {
+			schema := tb.Schema(false).(*catalog.Schema)
+			if schema.HasPK() {
+				pkDef := schema.GetSingleSortKey()
+				idx := pkDef.Idx
+				isCompositeKey := pkDef.IsCompositeColumn()
+				for i := 0; i < req.Batch.Vecs[0].Length(); i++ {
+					if isCompositeKey {
+						pkbuf := req.Batch.Vecs[idx].GetBytesAt(i)
+						tuple, _ := types.Unpack(pkbuf)
+						logutil.Info(
+							"op1",
+							zap.String("txn", txn.String()),
+							zap.String("pk", common.TypeStringValue(*req.Batch.Vecs[idx].GetType(), pkbuf, false)),
+							zap.Any("detail", tuple.SQLStrings(nil)),
+						)
+					} else {
+						logutil.Info(
+							"op1",
+							zap.String("txn", txn.String()),
+							zap.String("pk", common.MoVectorToString(req.Batch.Vecs[idx], i)),
+						)
+					}
+				}
+			}
+
+		}
 		//Appends a batch of data into table.
 		err = AppendDataToTable(ctx, tb, req.Batch)
 		return
@@ -1032,33 +1033,33 @@ func (h *Handle) HandleWrite(
 	//defer pkVec.Close()
 	// TODO: debug for #13342, remove me later
 	//_, _, injected := fault.TriggerFault(objectio.FJ_CommitDelete)
-	//if h.IsInterceptTable(tb.Schema(false).(*catalog.Schema).Name) || injected {
-	//	schema := tb.Schema(false).(*catalog.Schema)
-	//	if schema.HasPK() {
-	//		rowids := vector.MustFixedColNoTypeCheck[types.Rowid](rowIDVec.GetDownstreamVector())
-	//		isCompositeKey := schema.GetSingleSortKey().IsCompositeColumn()
-	//		for i := 0; i < len(rowids); i++ {
-	//			if isCompositeKey {
-	//				pkbuf := req.Batch.Vecs[1].GetBytesAt(i)
-	//				tuple, _ := types.Unpack(pkbuf)
-	//				logutil.Info(
-	//					"op2",
-	//					zap.String("txn", txn.String()),
-	//					zap.String("pk", common.TypeStringValue(*req.Batch.Vecs[1].GetType(), pkbuf, false)),
-	//					zap.String("rowid", rowids[i].String()),
-	//					zap.Any("detail", tuple.SQLStrings(nil)),
-	//				)
-	//			} else {
-	//				logutil.Info(
-	//					"op2",
-	//					zap.String("txn", txn.String()),
-	//					zap.String("pk", common.MoVectorToString(req.Batch.Vecs[1], i)),
-	//					zap.String("rowid", rowids[i].String()),
-	//				)
-	//			}
-	//		}
-	//	}
-	//}
+	if h.IsInterceptTable(tb.Schema(false).(*catalog.Schema).Name) {
+		schema := tb.Schema(false).(*catalog.Schema)
+		if schema.HasPK() {
+			rowids := vector.MustFixedColNoTypeCheck[types.Rowid](rowIDVec.GetDownstreamVector())
+			isCompositeKey := schema.GetSingleSortKey().IsCompositeColumn()
+			for i := 0; i < len(rowids); i++ {
+				if isCompositeKey {
+					pkbuf := req.Batch.Vecs[1].GetBytesAt(i)
+					tuple, _ := types.Unpack(pkbuf)
+					logutil.Info(
+						"op2",
+						zap.String("txn", txn.String()),
+						zap.String("pk", common.TypeStringValue(*req.Batch.Vecs[1].GetType(), pkbuf, false)),
+						zap.String("rowid", rowids[i].String()),
+						zap.Any("detail", tuple.SQLStrings(nil)),
+					)
+				} else {
+					logutil.Info(
+						"op2",
+						zap.String("txn", txn.String()),
+						zap.String("pk", common.MoVectorToString(req.Batch.Vecs[1], i)),
+						zap.String("rowid", rowids[i].String()),
+					)
+				}
+			}
+		}
+	}
 	err = tb.DeleteByPhyAddrKeys(rowIDVec, pkVec, handle.DT_Normal)
 	return
 }
