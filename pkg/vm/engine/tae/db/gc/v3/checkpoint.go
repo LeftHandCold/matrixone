2d340fb407bde253274f5c0665bb7e0732f8de65
GreatRiver <great.river703@gmail.com>
2025-06-30 17:54:32 +0800

    Update code

--- pkg/vm/engine/tae/db/gc/v3/checkpoint.go
+++ pkg/vm/engine/tae/db/gc/v3/checkpoint.go
@@ -1741,8 +1741,12 @@ func (c *checkpointCleaner) scanCheckpointsLocked(
 		saveSnapshot,
 		memoryBuffer,
 	); err != nil {
-		gcWindow.Close()
-		gcWindow = nil
+		if !moerr.IsMoErrCode(err, moerr.ErrFileNotFound) {
+			gcWindow.Close()
+			gcWindow = nil
+			return
+		}
+		err = nil
 		return
 	}
 	newFiles = append(newFiles, ioutil.MakeFullName(gcWindow.dir, gcMetaFile))
