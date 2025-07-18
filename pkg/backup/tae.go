8e65cb6d591ba39aa4ce078a8a55e25284c7ccda
GreatRiver <great.river703@gmail.com>
2025-07-18 11:28:03 +0800

    Update

--- pkg/backup/tae.go
+++ pkg/backup/tae.go
@@ -70,7 +70,7 @@ func getFileNames(ctx context.Context, retBytes [][][]byte) ([]string, error) {
 				continue
 			}
 			fileName = append(fileName, x)
-			logutil.Infof("fileName is %v", fileName)
+			logutil.Infof("fileName is %v", x)
 		}
 	}
 	return fileName, err
