30fdea505bc3f2627901e31a347b4b22d9dc3192
GreatRiver <great.river703@gmail.com>
2025-10-23 19:50:17 +0800

    U

--- pkg/vm/engine/ckputil/meta.go
+++ pkg/vm/engine/ckputil/meta.go
@@ -177,8 +177,10 @@ func ExportToTableRangesByFilter(
 	startRows := vector.MustFixedColNoTypeCheck[types.Rowid](data.Vecs[2])
 	endRows := vector.MustFixedColNoTypeCheck[types.Rowid](data.Vecs[3])
 	for i, rows := start, data.RowCount(); i < rows; i++ {
-		test := objectio.ObjectStats(data.Vecs[4].GetBytesAt(i))
-		//logutil.Infof("tableIds[i] is %d, tableId is %d, objectTypes[i] is %v, oo %v, startRows[i] is %v, endRows[i is %v ", tableIds[i], tableId, objectTypes[i], objectType, startRows[i].String(), endRows[i].String())
+		//test := objectio.ObjectStats(data.Vecs[4].GetBytesAt(i))
+		//if test.ObjectName().String() == "019a0fde-8f79-7c38-aa21-e280d9d377fd_00000" {
+		//	logutil.Infof("tableIds[i] is %d, tableId is %d, objectTypes[i] is %v, oo %v, startRows[i] is %v, endRows[i is %v ", tableIds[i], tableId, objectTypes[i], objectType, startRows[i].String(), endRows[i].String())
+		//}
 		//if tableIds[i] != tableId || objectTypes[i] != objectType {
 		//	continue
 		//}
