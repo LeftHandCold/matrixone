c89c7e999262466906ac64ef7893e22a6c1ead0f
GreatRiver <great.river703@gmail.com>
2026-01-22 18:04:31 +0800

    U

--- pkg/sql/plan/bind_replace.go
+++ pkg/sql/plan/bind_replace.go
@@ -20,7 +20,6 @@ import (
 
 	"github.com/matrixorigin/matrixone/pkg/catalog"
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
-	"github.com/matrixorigin/matrixone/pkg/logutil"
 	"github.com/matrixorigin/matrixone/pkg/pb/plan"
 	"github.com/matrixorigin/matrixone/pkg/sql/parsers/tree"
 	"github.com/matrixorigin/matrixone/pkg/sql/util"
@@ -37,9 +36,7 @@ func (builder *QueryBuilder) bindReplace(stmt *tree.Replace, bindCtx *BindContex
 	// because it needs to scan the entire table for each batch.
 	if valuesClause, ok := stmt.Rows.Select.(*tree.ValuesClause); ok {
 		rowCount := len(valuesClause.Rows)
-		logutil.Infof("bindReplace: rowCount=%d, threshold=%d", rowCount, replaceRowThreshold)
 		if rowCount > replaceRowThreshold {
-			logutil.Infof("bindReplace: switching to buildReplace path due to large batch")
 			return 0, moerr.NewUnsupportedDML(builder.GetContext(), "large batch replace (use DELETE+INSERT path)")
 		}
 	}
