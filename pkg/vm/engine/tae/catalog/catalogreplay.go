8f0aceba13ef65155a7fe4172d1e1410e0447229
GreatRiver <great.river703@gmail.com>
2026-02-25 11:33:55 +0800

    u

--- pkg/vm/engine/tae/catalog/catalogreplay.go
+++ pkg/vm/engine/tae/catalog/catalogreplay.go
@@ -491,6 +491,7 @@ func (catalog *Catalog) ReplayMOTables(ctx context.Context, txnNode *txnbase.Txn
 
 func (catalog *Catalog) onReplayCreateTable(dbid, tid uint64, schema *Schema, txnNode *txnbase.TxnMVCCNode) {
 	catalog.OnReplayTableID(tid)
+	logutil.Infof("dbid is %d, tid is %d", dbid, tid)
 	db, err := catalog.GetDatabaseByID(dbid)
 	if err != nil {
 		panic(err)
